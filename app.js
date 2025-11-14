be4ff4cd365708137e7e508809101cb374169bf0
Ashley Sands <ashley.sands01@gmail.com>
2025-11-14 11:23:02 -0800

    pseudocode and basic setup

---
+++ app.js
@@ -0,0 +1,17 @@
+// user presses start to begin the game
+// a random word from an array of words is shown
+// a timer for 30 seconds starts
+// when the timer ends the user can play again to go to a new rounds or end the game
+// when the user ends the game, they see all the words they went through
+
+/*-------------------------------- Constants --------------------------------*/
+const wordList = ['el libro', 'el gato', 'el perro']
+
+/*---------------------------- Variables (state) ----------------------------*/
+let randomWord
+
+/*------------------------ Cached Element References ------------------------*/
+
+/*-------------------------------- Functions --------------------------------*/
+
+/*----------------------------- Event Listeners -----------------------------*/
