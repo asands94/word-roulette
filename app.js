3c542bfb06e0246934b5ecfe3cf4903cbcab2bb3
Ashley Sands <ashley.sands01@gmail.com>
2025-11-18 19:12:27 -0800

    generate a random word

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
 /*-------------------------------- Constants --------------------------------*/
-const words = ['el libro', 'el baño', 'el hombre']
 
 /*---------------------------- Variables (state) ----------------------------*/
+let wordsList
 let timeLeft
 let usedWords
 let countdown
+let randomWord
 
 /*------------------------ Cached Element References ------------------------*/
 const startButton = document.querySelector('#start')
@@ -14,6 +15,8 @@ const word = document.querySelector('#word')
 /*-------------------------------- Functions --------------------------------*/
 function init() {
     timeLeft = 5
+    wordsList = ['el libro', 'el baño', 'el hombre']
+    usedWords = []
     // reset timer back to 60 secs
     // show the game description
     // show the start button
@@ -22,12 +25,22 @@ function init() {
 }
 
 function generateRandomWord() {
+    if (wordsList.length === 0) return
+    // generate the random word
+    randomWord = wordsList[Math.floor(Math.random() * wordsList.length)]
+    // push the word into the used words array
+    usedWords.push(randomWord)
+    // find the index of the random word
+    const indexOfWord = wordsList.indexOf(randomWord)
+    // remove the random word from the word list array
+    wordsList.splice(indexOfWord, 1)
     // generate a random word
     // remove the last shown word from the word array and put it in the used words array
 }
 
 function start() {
     setTimer()
+    generateRandomWord()
     // start the timer - 60 seconds
     // show a random word from the words array
     // hide the start button
@@ -38,7 +51,6 @@ function setTimer() {
     countdown ??= setInterval(() => {
         if (timeLeft === 0) return
         timeLeft -= 1
-        console.log(timeLeft)
     }, 1000)
 }
 
