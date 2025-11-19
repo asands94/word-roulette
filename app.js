89b3b633c4c6b4a079d07e7df7e70026bb7109bd
Ashley Sands <ashley.sands01@gmail.com>
2025-11-18 18:45:18 -0800

    add elements to html and cached them

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
 /*-------------------------------- Constants --------------------------------*/
+const words = ['el libro', 'el baño', 'el hombre']
 
 /*---------------------------- Variables (state) ----------------------------*/
 
 /*------------------------ Cached Element References ------------------------*/
+const startButton = document.querySelector('#start')
+const timer = document.querySelector('#timer')
+const word = document.querySelector('#word')
 
 /*-------------------------------- Functions --------------------------------*/
 
